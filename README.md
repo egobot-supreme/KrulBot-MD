601743c990898ea985c7153bd2b1dc77420f93bc
Señor Hekyzde <132515624+egobot-supreme@users.noreply.github.com>
2023-10-12 15:45:57 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# KrulBot-MD
+### INSTALACIÓN MANUAL
+
+```bash
+termux-setup-storage
+```
+```bash
+apt update
+```
+```bash
+apt upgrade
+```
+```bash
+pkg install -y git nodejs ffmpeg imagemagick yarn
+```
+```bash
+git clone https://github.com/egobot-supreme/KrulBot-MD 
+```
+```bash
+cd KrulBot-MD
+```
+```bash
+yarn install
+```
+```bash
+npm install
+```
+```bash
+npm start
+```
+### CREADOR 
+[![egobot-supreme](https://github.com/egobot-supreme.png?size=100)](https://github.com/egobot-supreme) 
---
+++ config.js
@@ -0,0 +1,206 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone'
+import { en, es, id, ar, pt } from './lib/idiomas/total-idiomas.js'
+
+// ES ➜ Agregué primero el número del Bot como prioridad
+// ES ➜ Si desea recibir reportes debe de usar los tres parámetros (Número, nombre y true)
+// EN ➜ Add the Bot number as priority first
+// EN ➜ If you want to receive reports you must use the three parameters (Number, name and true)
+global.owner = [
+['5493873452653', '𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 𝘾𝙧𝙚𝙖𝙙𝙤𝙧𝙖 💕🍡💕', true],
+['5493873452654', '𝙊𝙬𝙣𝙚𝙧 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', true], 
+['51947454367', '𝙊𝙬𝙣𝙚𝙧 𝙈𝙖𝙣𝙜𝙖𝙧𝙖 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', true],
+['5493873687620', '𝙊𝙬𝙣𝙚𝙧 𝙎𝙚𝙜𝙪𝙣𝙙𝙤 𝙎𝙪𝙥𝙚𝙧𝙞𝙤𝙧 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', true],
+['19147094259', '𝙊𝙬𝙣𝙚𝙧 𝙅𝙚𝙛𝙚 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', true],
+['5493873388067', '𝙊𝙬𝙣𝙚𝙧 𝙋𝙧𝙞𝙢𝙚𝙧 𝙎𝙪𝙥𝙚𝙧𝙞𝙤𝙧 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', true],
+['51958907754', '𝙊𝙬𝙣𝙚𝙧 𝙇𝙞𝙯 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', true],
+['5493873452636', '𝙊𝙬𝙣𝙚𝙧 𝙊𝙘𝙩𝙖𝙫𝙤 𝙎𝙪𝙥𝙚𝙧𝙞𝙤𝙧 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', true],
+['5493873388102', '𝙊𝙬𝙣𝙚𝙧 𝘿𝙚𝙘𝙞𝙢𝙤 𝙎𝙪𝙥𝙚𝙧𝙞𝙤𝙧 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', true]
+]
+
+global.mods = [] 
+global.prems = []
+   
+// ES ➜ Agregue el código de idioma el cual usará egoBot  
+// EN ➜ Add the language code which egoBot will use
+//  es = Español      id = Bahasa Indonesia       ar = عرب
+//  en = English      pt = Português 
+global.lenguajeEB = es  //<-- Predeterminado en idioma Español 
+
+// confirmación en el ajuste de idioma y owner en la consola
+global.languageLen = ""
+global.registerNumber = ""
+
+// ES ➜ Consigue Apikey en https://platform.openai.com/account/api-keys
+global.openai_key = 'sk-0'
+
+// ES ➜ Consigue tu ID de organizacion en: https://platform.openai.com/account/org-settings
+global.openai_org_id = 'org-3'
+
+global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = 'egoDios'
+global.itsrose = ['4b146102c4d500809da9d1ff']
+global.baileys = '@whiskeysockets/baileys'
+
+global.APIs = { 
+  xteam: 'https://api.xteam.xyz',
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://api.zahwazein.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',	
+  fgmods: 'https://api-fgmods.ddns.net',
+  botcahx: 'https://api.botcahx.biz.id',
+  ibeng: 'https://api.ibeng.tech/docs',	
+  rose: 'https://api.itsrose.site',
+  popcat : 'https://api.popcat.xyz',
+  xcoders : 'https://api-xcoders.site'
+},
+global.APIKeys = { 
+  'https://api.xteam.xyz': `${keysxteam}`,
+  'https://api.lolhuman.xyz': `${lolkeysapi}`,
+  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://violetics.pw': 'beta',
+  'https://api.zahwazein.xyz': `${keysxxx}`,
+  'https://api-fgmods.ddns.net': 'fg-dylux',
+  'https://api.botcahx.biz.id': 'Admin',
+  'https://api.ibeng.tech/docs': 'tamvan',
+  'https://api.itsrose.site': 'Rs-Zeltoria',
+  'https://api-xcoders.site': 'Frieren'
+}
+
+global.mods = [] 
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment	
+
+global.packname = ' ⏤͟͟͞͞🅺𝗿͢𝘂𝗹︎͢🅱︎𝗼͢𝘁-🅼͢🅳꒱⸼ᬽ︎︎'
+global.author = '𝑨𝒎𝒚 𝑶𝒇𝒊𝒄𝒊𝒂𝒍'
+
+// ES ➜ Está parte es para mostrar el contacto de alguien al usar #contacto
+// EN ➜ This part is to display someone's contact using #contact
+global.official = [ 
+['5493873452653', '𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 𝘾𝙧𝙚𝙖𝙙𝙤𝙧𝙖 💕🍡💕', 1],
+['5493873452654', '𝙊𝙬𝙣𝙚𝙧 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', 1], 
+['51947454367', '𝙊𝙬𝙣𝙚𝙧 𝙈𝙖𝙣𝙜𝙖𝙧𝙖 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', 1],
+['5493873687620', '𝙊𝙬𝙣𝙚𝙧 𝙎𝙚𝙜𝙪𝙣𝙙𝙤 𝙎𝙪𝙥𝙚𝙧𝙞𝙤𝙧 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', 1],
+['19147094259', '𝙊𝙬𝙣𝙚𝙧 𝙅𝙚𝙛𝙚 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', 1],
+['5493873388067', '𝙊𝙬𝙣𝙚𝙧 𝙋𝙧𝙞𝙢𝙚𝙧 𝙎𝙪𝙥𝙚𝙧𝙞𝙤𝙧 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', 1],
+['51958907754', '𝙊𝙬𝙣𝙚𝙧 𝙇𝙞𝙯 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', 1],
+['5493873452636', '𝙊𝙬𝙣𝙚𝙧 𝙊𝙘𝙩𝙖𝙫𝙤 𝙎𝙪𝙥𝙚𝙧𝙞𝙤𝙧 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', 1],
+['5493873388102', '𝙊𝙬𝙣𝙚𝙧 𝘿𝙚𝙘𝙞𝙢𝙤 𝙎𝙪𝙥𝙚𝙧𝙞𝙤𝙧 𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿 💞💞', 1]
+]
+
+global.mail = '' //agrega tú correo
+global.desc = '' //agrega una descripción corta
+global.desc2 = '' //agrega una descripción larga (Solo se aplicará si su whasapp no tiene descripción)
+global.country = '' //agrega tú país ejemplo: 🇪🇨
+
+global.vs = '1.5.0'
+global.vsJB = '4.5 (Beta)'
+
+global.krulnameofc = '𝙆𝙧𝙪𝙡𝘽𝙤𝙩-𝙈𝘿'
+global.yt = 'https://youtube.com/@egodios'
+global.yt2 = 'https://www.youtube.com/watch?v=Ko019wvu2Tc&t=71s'
+global.ig = 'https://www.instagram.com/ego_dios'
+global.md = 'https://github.com/egoNina-Li/egoBotLite-MD'
+global.fb = 'https://www.facebook.com/groups/872989990425789/'
+
+global.n2 = 'https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A' //CANAL egoBOT
+global.nna = 'https://chat.whatsapp.com/DVhu9S9Zr7cBFFl26N61U3' //UPDATE egoBOT
+global.nn2 = 'https://chat.whatsapp.com/HLXJUIznsbHJ3LF6pTgmh6' //UPDATE 2
+global.nna2 = 'https://chat.whatsapp.com/JRFOdyjNqhwEJcDQoBCHzL' //Help
+global.nn = 'https://chat.whatsapp.com/Ii0hagX0idw4C7Gxwts54x' //Grupo 1
+global.nnn = 'https://chat.whatsapp.com/LGHW0ZwLgpeH7Nsn4vlrWi' //Grupo 2
+global.nnnt = 'https://chat.whatsapp.com/Jvhbjxueer0GSdOw75ZtWR' //Grupo 3
+global.nnntt = 'https://chat.whatsapp.com/Gysm0PMJ4zg7ts0E9t91Hz' //Grupo 4
+global.nnnttt = 'https://chat.whatsapp.com/IZeaMQos8pxEhlyVI6yZaa' //Grupo 5
+global.nnnttt1 = 'https://chat.whatsapp.com/B8lrwGzSUbf7zlZHDAwCQ8' //Grupo 6 COL
+global.nnnttt2 = 'https://chat.whatsapp.com/IlyJBy1fxcw2qAHLSC5YLa' //Grupo 7 COL
+global.nnnttt3 = 'https://chat.whatsapp.com/LVocHnQjrci6dM4TZdmhBH' //Grupo 8 COL
+global.nnnttt4 = 'https://chat.whatsapp.com/KLpYFsitkgzJ15YQxZrBqO' //Grupo 9 COL
+global.nnnttt5 = 'https://chat.whatsapp.com/HOCsvLox0Ui7cwzTCeFhPP' //A.T.M.M
+global.paypal = 'https://paypal.me/OficialGD'
+global.asistencia = 'Wa.me/19147094259' //Dudas? escríbeme...
+
+global.wm = ' ⏤͟͟͞͞🅺𝗿͢𝘂𝗹︎͢🅱︎𝗼͢𝘁-🅼͢🅳꒱⸼ᬽ︎︎ : 🍰⃟ཱི࿆𝙰𝚖𝚢Co͢nej͢itaᬽ🍰'
+global.igfg = ' ⏤͟͟͞͞🅺𝗿͢𝘂𝗹︎͢🅱︎𝗼͢𝘁-🅼͢🅳꒱⸼ᬽ︎︎'
+global.wait = lenguajeEB['smsMeWait']()
+global.wait2 = lenguajeEB.smsWait()
+global.nomorown = owner[0][0]
+
+global.imagen1 = fs.readFileSync('./media/menus/Menu3.jpg')
+global.imagen2 = fs.readFileSync('./media/menus/img1.jpg')
+global.imagen3 = fs.readFileSync('./media/menus/img2.jpg')
+global.imagen4 = fs.readFileSync('./media/menus/img3.jpg')
+global.imagen5 = fs.readFileSync('./media/menus/img4.jpg')
+global.imagen6 = fs.readFileSync('./media/menus/img5.jpg')
+global.imagen7 = fs.readFileSync('./media/menus/img6.jpg')
+global.imagen8 = fs.readFileSync('./media/menus/img7.jpg')
+global.imagen9 = fs.readFileSync('./media/menus/img8.jpg')
+global.imagen10 = fs.readFileSync('./media/menus/img9.jpg')
+global.imagen11 = fs.readFileSync('./media/menus/img10.jpg')
+global.imagen12 = fs.readFileSync('./media/menus/img11.jpg')
+global.imagen13 = fs.readFileSync('./media/menus/img12.jpg')
+
+global.img = 'https://i.imgur.com/AwlL9kc.jpeg'
+global.img2 = 'https://i.imgur.com/p18q1Ok.jpeg'
+global.img3 = 'https://i.imgur.com/01Z8a0a.jpg' //ft rectangular
+global.img5 = 'https://i.imgur.com/80uz37R.jpeg'
+global.img6 = 'https://i.imgur.com/3zSvnGY.jpeg'
+global.img7 = 'https://i.imgur.com/WY4r6up.jpeg'
+global.img8 = 'https://i.imgur.com/qCO3RYa.jpeg'
+global.img9 = 'https://i.imgur.com/dWk51FS.jpeg'
+global.img10 = 'https://i.imgur.com/T4NjKMi.jpeg'
+global.img11 = 'https://i.imgur.com/jqyWSlh.jpeg'
+global.img12 = 'https://i.imgur.com/mpCRttm.jpeg'
+global.img13 = 'https://i.imgur.com/O04epJI.jpeg'
+global.img14 = 'https://i.imgur.com/jfbuJRU.jpeg'
+global.img15 = 'https://i.imgur.com/DzqUXkW.jpeg'
+global.img17 = 'https://i.imgur.com/Y3ZWq7z.jpeg'
+global.img18 = 'https://i.imgur.com/kaUN1Nz.jpeg'
+global.img19 = 'https://i.imgur.com/7yJ22hJ.jpeg'
+global.img20 = 'https://i.imgur.com/qcD353P.jpeg'
+global.img21 = 'https://i.imgur.com/3fJTaX6.jpeg'
+global.img21 = 'https://i.imgur.com/akofqcE.jpeg' //paypal
+
+global.welego = [ig, yt2, yt2, ig, md, ig, yt, paypal, yt2, yt2, ig, fb]
+global.redesMenu = [n2, nna, nn, nnn, nnnt, nnntt, nnnttt, nnnttt1, nnnttt2, nnnttt3, nnnttt4, md, ig, paypal, yt, asistencia, fb]
+global.egoMenu = [img, img2, img6, img7, img8, img9, img13, img14, img15, img17, img18, img19, img20, img21]
+global.egoImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11, imagen12, imagen13]
+
+global.htki = '*⭑•̩̩͙⊱•••• ☪*'
+global.htka = '*☪ ••••̩̩͙⊰•⭑*'
+global.htjava = '⫹⫺'
+global.correct = '✅'
+global.fault = '💔'
+global.alert = '⚠️'
+global.sending = '📋'
+global.sent = '❇️'
+global.notsent = '❗'
+global.waitemot = '⌛'
+global.waitemot2 = '⏳'
+
+global.multiplier = 60 // Cuanto más alto, más difícil subir de nivel 
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+unwatchFile(file)
+console.log(chalk.bold.greenBright(lenguajeEB['smsConfigBot']().trim()))
+import(`${file}?update=${Date.now()}`)
+})
